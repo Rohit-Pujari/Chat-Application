c4827ca880b05c159a273626221ffe64b14c2a98
Rohit Pujari <84202428+Rohit-Pujari@users.noreply.github.com>
2024-05-31 05:20:44 +0000

    na

--- Chatapp/Django/chat/views.py
+++ Chatapp/Django/chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import *
+from django.contrib import messages
 # Create your views here.
 @login_required
 def chatlist(request):
